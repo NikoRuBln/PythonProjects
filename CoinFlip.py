561a8cf77a99cf468d3d6ba0e98cfc336e8a21b9
Nikolai Ruilova <ruilova31@gmail.com>
2024-01-04 23:07:59 +0100

    Minor corrections

--- CoinFlip.py
+++ CoinFlip.py
@@ -4,8 +4,8 @@ print("I will flip a coin 1000 times. Guess how many times it will come up heads
 input()
 flips = 0
 heads = 0
- while flips < 1000:
-     if random.randint(0,1) == 1:
+while flips < 1000:
+    if random.randint(0,1) == 1:
          heads = heads + 1
     flips = flips + 1
 
